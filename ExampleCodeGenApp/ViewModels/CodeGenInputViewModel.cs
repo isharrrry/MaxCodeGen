52600e3013f0d5533642c64754ea73fe3898094e
HuanLin Huang <xunemail@qq.com>
2024-08-16 09:01:02 +0800

    flow 类型连接验证

--- ExampleCodeGenApp/ViewModels/CodeGenInputViewModel.cs
+++ ExampleCodeGenApp/ViewModels/CodeGenInputViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ExampleCodeGenApp.ViewModels.Nodes;
 using ExampleCodeGenApp.Views;
+using NodeNetwork;
 using NodeNetwork.Toolkit.ValueNode;
 using NodeNetwork.ViewModels;
 using NodeNetwork.Views;
@@ -27,6 +29,20 @@ namespace ExampleCodeGenApp.ViewModels
             {
                 this.PortPosition = PortPosition.Right;
             }
+
+            ConnectionValidator = TypeConnectionValidator;
+        }
+
+        private ConnectionValidationResult TypeConnectionValidator(PendingConnectionViewModel pending)
+        {
+            var t1 = ((CodeGenPortViewModel)pending.Output.Port)?.PortType;
+            var t2 = ((CodeGenPortViewModel)Port)?.PortType;
+            var isValid = t1 == t2 || t1 >= PortType.Double && t2 >= PortType.Double;
+            if (!isValid)
+            {
+                Debug.WriteLine($"类型 {t1} 与 {t2} 不匹配!");
+            }
+            return new ConnectionValidationResult(isValid, null);
         }
 
         public NodePortConfig PortConfig { get; set; }
